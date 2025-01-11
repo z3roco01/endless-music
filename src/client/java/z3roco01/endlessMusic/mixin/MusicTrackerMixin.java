70f121fa98249972808d50c603f4018dac442ea6
z3roco01 <zerocool@e.email>
2025-01-11 01:45:19 -0600

    update to version 1.21.4

--- src/client/java/z3roco01/endlessMusic/mixin/MusicTrackerMixin.java
+++ src/client/java/z3roco01/endlessMusic/mixin/MusicTrackerMixin.java
@@ -1,6 +1,7 @@
 package z3roco01.endlessMusic.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.MusicInstance;
 import net.minecraft.client.sound.MusicTracker;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.sound.MusicSound;
@@ -18,12 +19,13 @@ public abstract class MusicTrackerMixin {
     @Shadow @Final private MinecraftClient client;
     @Shadow @Nullable private SoundInstance current;
     @Shadow private int timeUntilNextSong;
-    @Shadow public abstract void play(MusicSound type);
+
+    @Shadow public abstract void play(MusicInstance music);
 
     @Inject(at = @At("HEAD"), method = "tick", cancellable = true)
     public void tick(CallbackInfo ci) {
-        MusicSound musicSound = this.client.getMusicType();
-        if (this.current != null) {
+        MusicSound musicSound = this.client.getMusicInstance().music();
+        if (this.current != null && musicSound != null) {
             if (!(musicSound.getSound().value()).id().equals(this.current.getId()) && musicSound.shouldReplaceCurrentMusic()) {
                 this.client.getSoundManager().stop(this.current);
                 this.timeUntilNextSong = EndlessMusic.getDelaySeconds();
@@ -35,9 +37,9 @@ public abstract class MusicTrackerMixin {
             }
         }
 
-        this.timeUntilNextSong = Math.min(this.timeUntilNextSong, musicSound.getMaxDelay() + EndlessMusic.getDelaySeconds());
+        this.timeUntilNextSong = Math.min(this.timeUntilNextSong, (musicSound == null ? 0 : musicSound.getMaxDelay()) + EndlessMusic.getDelaySeconds());
         if (this.timeUntilNextSong-- <= 0 && this.current == null) {
-            this.play(musicSound);
+            this.play(new MusicInstance(musicSound));
         }
 
         ci.cancel();
